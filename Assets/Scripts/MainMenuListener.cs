3fa8e50aafbaa86018ad5ab07a29b42a43b7f27d
Thunder <p.axelfranz@gmail.com>
2026-01-31 19:34:40 +0100

    All cops are beautiful

--- Assets/Scripts/MainMenuListener.cs
+++ Assets/Scripts/MainMenuListener.cs
@@ -5,15 +5,20 @@ public class MainMenuListener : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private Button m_startButton;
     [SerializeField] private Button m_creditsButton;
-    [SerializeField] private Button m_backButton;
+    [SerializeField] private Button m_controlsButton;
+    [SerializeField] private Button m_backCreditsButton;
+    [SerializeField] private Button m_backCommButton;
     [SerializeField] private GameObject m_mainMenu;
     [SerializeField] private GameObject m_credits;
+    [SerializeField] private GameObject m_commandes;
 
 
     void Start()
     {
         m_creditsButton.onClick.AddListener(switchToCredits);
-        m_backButton.onClick.AddListener(backToMenu);
+        m_controlsButton.onClick.AddListener(switchToControls);
+        m_backCreditsButton.onClick.AddListener(backToMenu);
+        m_backCommButton.onClick.AddListener(backToMenu);
         m_startButton.onClick.AddListener(startGame);
         GameManager.Instance.ChangeState(GameState.Menu);
     }
@@ -24,6 +29,13 @@ public class MainMenuListener : MonoBehaviour
         m_credits.SetActive(true);
     }
 
+    private void switchToControls()
+    {
+        m_mainMenu.SetActive(false);
+        m_commandes.SetActive(true);
+    }
+
+
     private void startGame()
     {
         m_mainMenu.SetActive(false);
@@ -33,6 +45,7 @@ public class MainMenuListener : MonoBehaviour
     private void backToMenu()
     {
         m_credits.SetActive(false);
+        m_commandes.SetActive(false);
         m_mainMenu.SetActive(true);
     }
 
