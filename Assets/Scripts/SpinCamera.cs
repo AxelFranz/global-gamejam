a9b6890a0ee977590afe58a37263874eb0da1ece
Thunder <p.axelfranz@gmail.com>
2026-02-01 14:31:57 +0100

    Mask Prefab + good pedestals

--- Assets/Scripts/SpinCamera.cs
+++ Assets/Scripts/SpinCamera.cs
@@ -5,7 +5,7 @@ public class SpinCamera : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Update()
     {
-        transform.RotateAround(Player.Instance.transform.position, Vector3.up, -20 * Time.deltaTime);
+        transform.RotateAround(Player.Instance.transform.position, Vector3.up, -40 * Time.deltaTime);
         transform.LookAt(Player.Instance.transform);
     }
 }
